0aa62a1bc9ffd8e9c8fb40b9e34276d6c18dc630
Lukasz <srednickilukasz@gmail.com>
2021-05-17 19:50:05 +0200

    Update

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
-// draw numbers
-function getRandomNumbers(min, max, q) {
-    let randomNumbers = [];
-
-    for (let i = 0; i < q; i++) {
-        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        randomNumbers.push(randomNumber);
-    }
-
-    let stringOutput = randomNumbers.join(', ');
-
-    return stringOutput;
+// Get random integer
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// press button to draw numbers
-document.querySelector('.draw-numbers').addEventListener('click', () => {
-    const min = parseInt(document.querySelector('.min').value);
-    const max = parseInt(document.querySelector('.max').value);
-    const q = parseInt(document.querySelector('.q').value);
+// Get random integers
+function getRandomIntegers(min, max, len) {
+    let integers = [];
 
-    document.querySelector('.numbers').innerText = getRandomNumbers(min, max, q);
+    if (max - min + 1 < len) {
+        while (integers.length < len) {
+            integers.push(getRandomInteger(min, max));
+        }
+    }
+    else {
+        while (integers.length < len) {
+            integers.push(getRandomInteger(min, max));
+            integers = Array.from(new Set(integers));
+        }
+    }
+
+    return integers.join(', ');
+}
+
+// Create alert with random integers
+document.querySelector('.draw').addEventListener('click', () => {
+    const min = document.querySelector('.min').value;
+    const max = document.querySelector('.max').value;
+    const len = document.querySelector('.len').value;
+
+    if (min === '' || max === '') {
+        alert('Invalid value for minumum or maximum');
+    }
+    else {
+        const main = document.querySelector('main');
+        const newDiv = document.createElement('div');
+
+        const intMin = parseInt(min);
+        const intMax = parseInt(max);
+        const intLen = parseInt(len);
+
+        intMax - intMin + 1 < intLen
+            ? newDiv.className = 'alert alert-danger alert-dismissible mb-2'
+            : newDiv.className = 'alert alert-success alert-dismissible mb-2';
+
+        newDiv.innerHTML = `
+            <button type='button' class='close h-100' style='font-size:1em;' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden="true">&times;</span>
+            </button>
+            ${getRandomIntegers(intMin, intMax, intLen)}
+        `;
+
+        main.insertBefore(newDiv, main.firstChild);
+    }
 });
